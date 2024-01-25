c9ef11f56f0377e9dd7ae11f8ee3280f504d41eb
ARCHANA HARKE <53932661+anpanche@users.noreply.github.com>
2024-01-25 14:47:51 +0100

    Update QCQA_3.py

--- Alleleome/QCQA_3.py
+++ Alleleome/QCQA_3.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from Bio import SeqIO
 import logging
+import os
 
 def process_sequences(pangenome_alignments_dir_path,alleleome_dir_path):
     """
@@ -13,13 +14,13 @@ def process_sequences(pangenome_alignments_dir_path,alleleome_dir_path):
     try:
         logging.info("Starting process_sequences in QCQA_3")
 
-        df = pd.read_csv(alleleome_dir_path + 'core_alleles_with_length_less_than_2std_less_than_mean_length.csv')
+        df = pd.read_csv(os.path.join(alleleome_dir_path, 'core_alleles_with_length_less_than_2std_less_than_mean_length.csv'))
         gene_locus_list = set(df['Gene'].to_list())
         locus_list = df['Locus_tag'].to_list()
     
         for file_type in ['fna', 'faa']:
             for gene in gene_locus_list:
-                allele_path = pangenome_alignments_dir_path + gene + '/input/'
+                allele_path = pangenome_alignments_dir_path +'/'+ gene + '/input/'
                 seq_file = allele_path + f'pangenes.{file_type}'
                 new_file = allele_path + f'pan_genes.{file_type}'
                 with open(seq_file, 'r') as file_in, open(new_file, 'w') as file_out:
